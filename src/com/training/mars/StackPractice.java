4f27f4ddddba65b9a367010913ea17e4bcae43fe
Heidi <heidi.willden@me.com>
2022-04-14 17:19:36 -0500

    Sort array in ascending order

--- src/com/training/mars/StackPractice.java
+++ src/com/training/mars/StackPractice.java
@@ -6,10 +6,23 @@ public class StackPractice {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter array length");
+		System.out.println("Enter the desired number of inputs.");
+		
 		int arrayLength = scanner.nextInt();
 		int [] array = new int[arrayLength];
-		System.out.println("Enter that many items into the array.");
+		
+		System.out.println("Enter that many numbers.");
+		
+	
+		
+	    for(int i=0; i<array.length; i++ ) {
+	         array[i] = scanner.nextInt();
+	      }
+	    
+	    System.out.println("These are the numbers you entered: " + Arrays.toString(array));
+
+		Arrays.sort(array);	
+	    System.out.println("Here are those numbers in ascending order: " + Arrays.toString(array));
 
 	}
 
